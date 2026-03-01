a2c5a1f870b6da45251a86bd63eb396ed36951a9
Anirudh Singh Rathore <anirudhsinghr@gmail.com>
2026-03-01 14:41:18 +0530

    Add build.bat file to allow builds on windows systems

---
+++ build.bat
@@ -0,0 +1,7 @@
+SET SPEC_FILE=timesheet_generator.spec
+
+IF EXIST %SPEC_FILE% (
+  pipenv run pyinstaller $SPEC_FILE
+) else (
+  pipenv run pyinstaller --onefile --console --add-data "timesheet_template.xlsx:." --name "timesheet_generator" --clean main.py
+)
